a91c210eb82b30eafd6459b4f71b6852809107e5
LufDevOps <devops.hoanghagiang@gmail.com>
2024-05-14 07:34:27 +0700

    update duplicate task description, warning emty list

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-    "fmt"
-    "bufio"
-    "os"
-    "strconv"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	// "strings"
 )
 
 type Task struct {
@@ -20,9 +21,18 @@ func NewTaskSlice() *TaskSlice {
     return &TaskSlice{tasks: []Task{}}
 }
 
-func (ts *TaskSlice) AddTask(description string) {
-    ts.tasks = append(ts.tasks, Task{Description: description, Completed: false})
+func (tm *TaskSlice) AddTask(description string) {
+    for _, task := range tm.tasks {
+        if task.Description == description {
+            fmt.Println("Task description already exists.")
+            return
+        }
+    }
+    // If not duplicate, add the task
+    tm.tasks = append(tm.tasks, Task{Description: description, Completed: false})
+    fmt.Println("Task added successfully.")
 }
+
 
 func (ts *TaskSlice) CompleteTask(i int) {
     if i >= 0 && i < len(ts.tasks) {
@@ -61,8 +71,19 @@ func main() {
             fmt.Print("Enter task description: ")
             scanner.Scan()
             description := scanner.Text()
-            TaskSlice.AddTask(description)
+			// for i := 0; i < len(TaskSlice.tasks); i++ {
+			// 	if TaskSlice.tasks[i].Description == strings.ToLower(description) {
+			// 		fmt.Println("Duplicate Task Description with Task: ",i+1,". ",TaskSlice.tasks[i].Description)
+			// 		continue
+			// 	}
+			// }
+			TaskSlice.AddTask(description)
+            
         } else if option == "2" {
+			if len(TaskSlice.tasks)==0 {
+				fmt.Println("!Task slice is empty, add task first")
+				continue
+			}
             fmt.Print("Please enter task id to complete: ")
             scanner.Scan()
             i, err := strconv.Atoi(scanner.Text())
